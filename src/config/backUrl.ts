305341fad99f7f5ec628e8d058cd0161f245fbec
kingheedo <52102550+kingheedo@users.noreply.github.com>
2024-03-15 20:57:27 +0900

    fix(backUrl): 경로 수정

--- src/config/backUrl.ts
+++ src/config/backUrl.ts
@@ -1,4 +1,3 @@
-// eslint-disable-next-line no-constant-condition
-const backUrl = process.env.NODE_ENV === 'development' ? 'http://api.next-react.shop' : 'localhost:3065';
+const backUrl = process.env.NODE_ENV === 'production' ? 'http://api.next-react.shop' : 'localhost:3065';
 
 export default backUrl;
