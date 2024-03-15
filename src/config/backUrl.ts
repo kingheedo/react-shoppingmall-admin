db7ccc1d61ad19ae183900ee2758d7b59998371c
kingheedo <52102550+kingheedo@users.noreply.github.com>
2024-03-15 19:39:50 +0900

    feat(빌드): pm2 ecosystem 추가 및 backUrl 수정

--- src/config/backUrl.ts
+++ src/config/backUrl.ts
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-constant-condition
-const backUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3065/' : 'http://localhost:3065/';
+const backUrl = process.env.NODE_ENV === 'development' ? 'http://api.next-react.shop' : 'localhost:3065';
 
 export default backUrl;
